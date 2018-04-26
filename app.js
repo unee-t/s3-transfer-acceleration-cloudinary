d06d87b22ee7cb9744df9c29497246d7c2ad3163
Kai Hendry <hendry@webconverger.com>
2018-04-26 10:06:23 +0800

    Added logger

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var AWS = require('aws-sdk')
 var config = { bucket: 'dev-media-unee-t' }
 
 // https:// docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/loading-node-credentials-shared.html
-// var credentials = new AWS.SharedIniFileCredentials({profile: 'uneet-dev'})
-// AWS.config.credentials = credentials
+var credentials = new AWS.SharedIniFileCredentials({profile: 'uneet-dev'})
+AWS.config.credentials = credentials
 
 AWS.config.region = 'ap-southeast-1'
 
@@ -26,7 +26,6 @@ app.get('/', function (req, res) {
 
   // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#createPresignedPost-property
   const presigned = S3.createPresignedPost(params)
-  // console.log(presigned)
 
   res.send(`<!DOCTYPE html>
     <html>
@@ -34,13 +33,15 @@ app.get('/', function (req, res) {
     <title>🚀Amazon S3 Transfer Acceleration</title>
     <meta name=viewport content="width=device-width, initial-scale=1">
     <meta name="robots" content="noindex">
+
     <style>
     body, input[type=submit] { font-family: "Helvetica Neue",Helvetica,Arial,sans-serif; font-size: x-large; }
     .inputs { display: flex; flex-direction: column; align-items: left; justify-content: left; }
     label { padding: 1em; margin: 0.3em; border: thick solid #0099CC; }
     </style>
+
     <script>
-    function fileSelected() {
+    async function fileSelected() {
 
       f = document.getElementById("file");
       var file = f.files[0];
@@ -49,7 +50,7 @@ app.get('/', function (req, res) {
       if (file) {
 
         if (file.name == "image.jpeg") {
-          // For IOS to have a unique filename
+          // For older IOS to have a unique filename
           var key = ymd + '/' + file.name.substring(0, file.name.lastIndexOf(".")) + Math.round(new Date().getTime() / 1000.0) + ".jpg";
         } else {
           var key = ymd + '/' + file.name;
@@ -79,16 +80,12 @@ app.get('/', function (req, res) {
 
       fd.append("file", f.files[0]);
 
-      fetch('https://${config.bucket}.s3-accelerate.amazonaws.com', { method: "POST", body: fd }).then(function (res) {
-        if (res.ok) {
+      // TODO: Track progress https://twitter.com/kaihendry/status/989316219747557376
+      const response = await fetch('https://${config.bucket}.s3-accelerate.amazonaws.com', { method: "POST", body: fd })
+      if (response.ok) {
           console.log("Successfully uploaded")
-        } else {
-          console.error("Fail", res)
-        }
-      }).catch(error => console.log(error) );
-
-        return false;
-    }
+        } else { console.log("Fail", response) }
+      }
         </script>
         </head>
         <body>
@@ -97,12 +94,20 @@ app.get('/', function (req, res) {
         <div id="fileSize"></div>
         <div id="fileType"></div>
 
+        <pre id="logger"></pre>
+
         <form class=inputs onsubmit="return fileSelected(this);">
         <label><strong>Optional:</strong> Upload file name
         <input type=text id=filename autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false"></label>
         <label>Upload file <input type=file required id=file></label>
         <input type=submit value="Upload">
         </form>
+
+        <!-- stupid logger from https://stackoverflow.com/a/45387558/4534 -->
+        <script>
+        !function(o){console.old=console.log,console.log=function(){var n,e,t="";for(e=0;e<arguments.length;e++)t+='<span class="log-'+typeof(n=arguments[e])+'">',"object"==typeof n&&"object"==typeof JSON&&"function"==typeof JSON.stringify?t+=JSON.stringify(n):t+=n,t+="</span>&nbsp;";o.innerHTML+=t+"<br>",console.old.apply(void 0,arguments)}}
+        (document.getElementById("logger"));
+        </script>
 
         </body>
         </html>`)
